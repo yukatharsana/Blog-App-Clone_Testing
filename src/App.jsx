92597931ee38c25b7d00a92bb646dcb36c421d4e
ThanushCorder126 <73095554+ThanuMahee12@users.noreply.github.com>
2024-01-03 20:02:56 +0530

    pages intial version and Setup routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Write from './pages/Write';
+import Single from './pages/Single';
+const router = createBrowserRouter([
+  {
+  path: '/home',
+  element:<Home/>
+},
+  {
+  path: '/',
+  element:<Login/>
+},
+  {
+  path: '/',
+  element:<Register/>
+},
+  {
+  path: '/',
+  element:<Write/>
+},
+  {
+  path: '/',
+  element:<Single/>
+},
 
+]);
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <RouterProvider router={router}/>
   )
 }
