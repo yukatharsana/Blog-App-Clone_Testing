19e7bc8725945abee091fa9a824b2dfc394a0b30
ThanushCorder126 <73095554+ThanuMahee12@users.noreply.github.com>
2024-01-09 16:06:18 +0530

    add

---
+++ blogapp/src/main/java/com/blog/blogapp/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.blog.blogapp.controller;
+
+import com.blog.blogapp.model.Users;
+import com.blog.blogapp.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/user")
+public class UserController {
+    @Autowired
+    private UserRepo userRepo;
+    @GetMapping
+    public List<Users> getall(){
+        return  userRepo.findAll();
+    }
+    @PostMapping
+    public Users newUser(Users user){
+return userRepo.save(user);
+    }
+    @GetMapping("{usrId}")
+    public ResponseEntity<Users> userById(@PathVariable  int userid){
+        Users user=userRepo.findById(userid)
+                .orElseThrow(()->new RuntimeException("Employee not exist with id"+userid));
+        return ResponseEntity.ok(user);
+    }
+    public  ResponseEntity<Users> updateUsers()
+}
