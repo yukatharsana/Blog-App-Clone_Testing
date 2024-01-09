55164a834fc132daacda4d45bcbcd84f2cbf0827
ThanushCorder126 <73095554+ThanuMahee12@users.noreply.github.com>
2024-01-09 15:27:09 +0530

    api added

---
+++ blogapp/src/main/java/com/blog/blogapp/model/Post.java
@@ -0,0 +1,7 @@
+package com.blog.blogapp.model;
+
+public class Post {
+private int postid;
+private String title;
+private String description;
+}
---
+++ blogapp/src/main/java/com/blog/blogapp/model/User.java
@@ -0,0 +1,25 @@
+package com.blog.blogapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="users")
+public class User {
+    @Id
+    @GeneratedValue()
+    private int userid;
+    private String email;
+    private String password;
+    private String name;
+}
