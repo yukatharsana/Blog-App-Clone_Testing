d8f03ad0327b331264faf4af9ac682908c6c20bb
ThanushCorder126 <73095554+ThanuMahee12@users.noreply.github.com>
2024-01-11 11:34:36 +0530

    post read more button

--- ui/src/component/Post.jsx
+++ ui/src/component/Post.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useSelector } from 'react-redux';
 import { postById } from '../Redux/Slice/PostSlice'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function BigPost ({ postid })
 {
     const { title,description,posturl} = useSelector(state => postById(state, postid))
-console.log(posturl.data);
+  console.log(posturl.data);
+  const navigate=useNavigate()
     return (
 
     <div className='post' key={postById}>
@@ -16,10 +17,10 @@ console.log(posturl.data);
   </div>
   <div className='content'>
     <Link className='link' to={`/post/${postid}`}>
-      <h1>{title} </h1>
+      <h1>{title}</h1>
     </Link>
     <p>{description}</p>
-    <button>Read More</button>
+    <button onClick={()=>navigate(`/post/${postid}`)} >Read More</button>
   </div>
 </div>
 
